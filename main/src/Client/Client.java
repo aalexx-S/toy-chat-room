e27c200fa9950b68e0dd18bdd21889b2ff32b850
aalexx <alex530519@gmail.com>
2016-01-10 23:15:54 +0800

    Added login page for client.

--- main/src/Client/Client.java
+++ main/src/Client/Client.java
@@ -1,17 +1,60 @@
 package Client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by aalexx on 1/10/16.
  */
 public class Client extends Application {
+    private Stage stage;
+
     public static void main (String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage.fxml"));
+        Parent root = loader.load();
+        LoginPageController controller = loader.getController();
+        /*
+         * do login/register stuff here
+         */
+        controller.setLoginOnAction(event -> loginSuccess());
 
+
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public void loginSuccess () {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        Parent root;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        MainPageController controller = loader.getController();
+        /*
+         * setup core and other functionality here
+         */
+
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(root, 800, 600);
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(root);
+        }
+        stage.sizeToScene();
     }
 }
