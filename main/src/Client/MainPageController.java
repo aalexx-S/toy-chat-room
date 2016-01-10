dbb9b47d16e25e09f88a7ba4b83cbe92bb7a2edc
aalexx <alex530519@gmail.com>
2016-01-10 23:17:49 +0800

    Added main page.
    Added main page.
    Containing two list of rooms.
    One is single and the other is multiple.
    There are also two MyPopupInputButton for the two type of room to find a
    people or to build a room.

---
+++ main/src/Client/MainPageController.java
@@ -0,0 +1,11 @@
+package Client;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by aalexx on 1/10/16.
+ */
+public class MainPageController {
+    @FXML private RoomList singleTable;
+    @FXML private RoomList multipleTable;
+}
